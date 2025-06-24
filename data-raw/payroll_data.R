d0a52484ce19528112b70e9d23c85f27959e9594
Hamish Gamble <hamish.gamble@amwu.org.au>
2025-06-24 11:05:06 +1000

    Delete files

--- data-raw/payroll_data.R
+++ data-raw/payroll_data.R
@@ -6,6 +6,10 @@ payroll_index <- read_payrolls("industry_jobs", path = "data-raw")
 payroll_index_industry <- read_payrolls("subindustry_jobs", path = "data-raw")
 payroll_index_business <- read_payrolls("empsize_jobs", path = "data-raw")
 
+file.remove(c("data-raw/6160055001_DO001.xlsx",
+              "data-raw/6160055001_DO002.xlsx",
+              "data-raw/6160055001_DO003.xlsx"))
+
 use_data(payroll_index, overwrite = TRUE, compress = "xz")
 use_data(payroll_index_industry, overwrite = TRUE, compress = "xz")
 use_data(payroll_index_business, overwrite = TRUE, compress = "xz")
