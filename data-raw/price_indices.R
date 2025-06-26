5b5e5f6df8feeee8ef66518751254f5c587c5870
Hamish Gamble <hamish.gamble@flinders.edu.au>
2025-06-26 13:38:30 +1000

    Compress data

--- data-raw/price_indices.R
+++ data-raw/price_indices.R
@@ -6,4 +6,10 @@ cpi_monthly <- read_abs("6484.0", tables = "1", retain_files = F) |>
   separate_series(column_names = c("indicator", "cpi_group", "state")) |> 
   select(date, indicator, cpi_group, state, unit, value)
 
-usethis::use_data(cpi_monthly, overwrite = TRUE)
+wpi_quarterly <- read_abs("6345.0", tables = paste0(c(2:5, 7:9), "b"), retain_files = F) |> 
+  separate_series(column_names = c("data_type", "indicator", "state", "sector", "industry")) |> 
+  select(date, data_type, indicator, state, sector, industry, unit, value)
+
+
+usethis::use_data(wpi_quarterly, compress = 'xz', overwrite = TRUE)
+usethis::use_data(cpi_monthly, compress = 'xz', overwrite = TRUE)
