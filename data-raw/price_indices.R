6adf7a3dd9ad35760592158ffa28874c9049157f
Hamish Gamble <hamish.gamble@amwu.org.au>
2025-07-01 15:21:38 +1000

    Index wpi to March 2020

--- data-raw/price_indices.R
+++ data-raw/price_indices.R
@@ -129,7 +129,10 @@ cpi_quarterly <- cpi_quarterly_expenditure_class |>
 
 wpi_quarterly <- read_abs("6345.0", tables = paste0(c(2:5, 7:9), "b"), retain_files = F) |> 
   separate_series(column_names = c("data_type", "indicator", "state", "sector", "industry")) |> 
-  select(date, data_type, indicator, state, sector, industry, unit, value)
+  select(date, data_type, indicator, state, sector, industry, unit, value) |>
+  group_by(data_type, indicator, state, sector, industry) |> 
+  mutate(reindex = 100*value/value[date == "2020-03-01"]) |> 
+  ungroup() 
 
 
 usethis::use_data(wpi_quarterly, compress = 'xz', overwrite = TRUE)
