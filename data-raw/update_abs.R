2bfa2f284dfed80c6f95936a7d3f9dd084f90626
Hamish Gamble <hamish.gamble@amwu.org.au>
2025-08-07 09:32:44 +1000

    Remove devtools import

--- data-raw/update_abs.R
+++ data-raw/update_abs.R
@@ -1,3 +1,4 @@
+library(devtools)
 source_files <- list.files("data-raw")
 source_files <- paste0("data-raw/", source_files[grep(".R", source_files)])
 #Exclude THIS file from being sourced - or else get stuck in a loop
@@ -8,7 +9,7 @@ source_files <- source_files[!grepl("data-raw/included_data.R", source_files)]
 #Update Data
 Map(source, source_files)
 #Document Data
-devtools::document()
+document()
 #Update included data
 source("data-raw/included_data.R")
 
