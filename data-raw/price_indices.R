3c48486e7195dd5cb3263ef917b91e66f3e49842
Hamish Gamble <hamish.gamble@amwu.org.au>
2025-08-08 14:47:36 +1000

    Add % change data to cpi quarterly

--- data-raw/price_indices.R
+++ data-raw/price_indices.R
@@ -108,8 +108,9 @@ cpi_monthly <- read_abs("6484.0", tables = "1", retain_files = F) |>
   separate_series(column_names = c("indicator", "cpi_group", "state")) |> 
   select(date, indicator, cpi_group, state, unit, value)
 
-cpi_quarterly_raw <- read_abs("6401.0", tables = "9", retain_files = F) |> 
+cpi_quarterly_raw <- read_abs("6401.0", tables = c(9, 10), retain_files = F) |> 
   separate_series(column_names = c("indicator", "expenditure_class", "state"), remove_nas = TRUE) 
+
 
 cpi_quarterly_group <- cpi_quarterly_raw |>
   filter(expenditure_class %in% c(cpi_groups$group, "All groups CPI")) |>
